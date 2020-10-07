432e57ac697f26e66742bd2b855e325f4342a068
michaelackerma <Michael.Ackerman-Pondexte@patelhs.org>
2020-10-07 13:13:49 +0000

    I added the beginning of the game

--- Main.java
+++ Main.java
@@ -1,5 +1,40 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("Pokemon");//red opening 
+    System.out.println("Press start ->");//Since I cant add images or music you have to use your imagination
+    boolean start= true;
+    boolean dontStart= false;
+    if(start != dontStart){
+      System.out.println("Welcome to the world of pokemon");
+
+    }else{
+      System.out.println("Press start or leave the game");
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   }
 }
