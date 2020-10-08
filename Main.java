e4b4af43a2a826c441f09298344070d26f7fba8a
michaelackerma <Michael.Ackerman-Pondexte@patelhs.org>
2020-10-08 18:13:29 +0000

    I added the character name... and rivals name and soon the player will choose their pokemon

--- Main.java
+++ Main.java
@@ -1,9 +1,16 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Pokemon");//red opening 
+    //opening//
+    System.out.println(" ");
+    System.out.println("-----Pokemon-----");//pokemon game opening 
+
+
+
+    //Pressing start
+    System.out.println(" ");
     System.out.println("Press start ->");//Since I cant add images or music you have to use your imagination
     boolean start= true;
-    boolean dontStart= false;
+    boolean dontStart= false;//this is not a 100% pokemon opening
     if(start != dontStart){
       System.out.println("Welcome to the world of pokemon");
 
@@ -11,7 +18,69 @@ class Main {
       System.out.println("Press start or leave the game");
 
     }
+    System.out.println("- Here in the world of pokemon you can find and catch many pokemon.     There are hundreds of them out there... are you ready");
+    System.out.println(" ");//for better code layout
+    System.out.println("-----Choose your name-----");
 
+
+
+    //choosing you name
+    boolean phill = false;//doing names will be difficult...
+    boolean maria = false;
+    boolean guest = true;
+    if(maria != phill){
+    System.out.println("Phill");
+
+    }else if(maria){
+    System.out.println("Maria");
+    } 
+    else if(maria == guest){
+    System.out.println("Maria");
+    }else{
+    System.out.println("Guest");
+    }
+    System.out.println("... Are you sure");
+    if(true == true){
+    System.out.println("yes");
+    }else{
+    System.out.println("then choose again");
+    }
+    System.out.println(" ");
+
+
+   
+  //dialouge after choosing name... then choosing your rival
+    System.out.println("Hello guest do you remember your old rival from middleschool... uhh ... I dont remember there name again... what is it? ");
+    System.out.println(" ");
+    //nothing fancy now that I did this before
+    boolean John = false;//doing names will be difficult...
+    boolean demetri = false;
+    boolean Christy = true;
+    if(maria != phill){
+    System.out.println("oh its... uh ... Joe ... Jacob? ... Oh I mean Jhon");
+
+    }else if(maria){
+    System.out.println("oh its... uh ... Dem ... Demi ... Oh I mean Demetri");
+    } 
+    else if(maria == guest){
+    System.out.println("oh its... uh ... Dem ... Demi? ... Oh I mean Demetri");
+    }else{
+    System.out.println("oh its... uh ... Chr ... Chris ... Oh I mean Christy");
+    }
+    System.out.println("... Are you sure");
+    if(true == true){
+    System.out.println("yes");
+    }else{
+    System.out.println("then choose again");
+    }
+
+    //now more story then I'll get to choosing pokemon
+    System.out.println("Now with this information go and become the worlds best pokemon trainer ...");
+
+    for(int i = 1; i <= 3; i++)
+    {
+    System.out.println(1 + "");
+    }
 
 
 
