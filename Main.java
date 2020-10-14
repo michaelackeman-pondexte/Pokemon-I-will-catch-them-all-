225b2c4fd32fb959cc01144ef8fcf629861d4dcd
michaelackerma <Michael.Ackerman-Pondexte@patelhs.org>
2020-10-14 14:59:27 +0000

    I added a finish allowed the person to choose pokemon and level. and alot of comments

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ class Main {
   public static void main(String[] args) {
     //opening//
     System.out.println(" ");
-    System.out.println("-----Pokemon-----");//pokemon game opening 
+    System.out.println("--------------------------------POKEMON---------------------------------");//pokemon game opening 
 
 
 
@@ -78,26 +78,80 @@ class Main {
 
 
 
+
     //Saving
-    System.out.println("Now with this information go and become the worlds best pokemon trainer.");
+    System.out.println("Now with this information go and recieve your pokemon.");
 
       for(int count = 1; count <= 3; count++)
      {
         System.out.println(count);
      }
     System.out.println("Saving...");
+    System.out.println(" ");
 
 
 
 
-    //Choosing pokemon this time ill use scanner
+
+    //Choosing pokemon and desired level this time ill use scanner
     Scanner scan = new Scanner(System.in);
     System.out.println("Now it's time to choose your pokemon.");
     System.out.print(" You have three choices Treeko, Torchic, or Mudkip. Which one will you  choose?");
-    String pokemon = scan.next();
+    String pokemon = scan.next();//I plan to use all main scanners
+    System.out.println(" ");//in this part of my code 
+    System.out.println("Is it true or false that you choose " + pokemon + " as your starting pokemon?");//if you type false nothing will make sense so please type true
+    boolean trueFalse = scan.nextBoolean();
     System.out.println(" ");
-    System.out.println("So you have chosen " + pokemon + " ?");
-  
+    System.out.println( trueFalse + ", No matter what you said you have choosen " + pokemon + " as your pokemon. If you want a different one close the game and start again");
+
+    System.out.println(" ");
+    System.out.println("A pokemons progression in abilities, strength, and obedience all require a high bond between trainer and pokemon."); 
+
+    System.out.println("The max level of pokemon can reach is 100. meaning that your pokemon has   the strongest abilities, strentgh, and obedience.");
+
+    System.out.println("The lowest level is 0  meaning your pokemon has no abilities, strength, or obdience.");
+    System.out.println(" ");
+    
+    System.out.println("What level of bond will you try to earn  with your pokemon?");
+    int level = scan.nextInt();
+    System.out.println("So you want to reach a level of " + level + " well I believe you can achieve that, unless it's above 100.");
+
+
+
+
+
+    //saving because that was alot of scanner
+    //after this save well set our adventurer into the 
+    //world of pokemon
+    System.out.println(" ");
+    System.out.println("Get ready... your pokemon adventure will soon begin");
+    int counting = 1;
+    while (counting < 4)
+    {
+      System.out.println(counting++);
+
+    }
+    System.out.println("Saving...");
+    System.out.println(" ");
+
+
+
+
+
+
+    //Now the epic conclusion of the opening sequence Finally
+    //Since the name isn't a scanner this will be weird
+    // So you dont get to choose your name(or rivals) really, but you can choose your pokemon... oof
+    //main variables to remember     guest, Christy, level, trueFalse, pokemon, and thats pretty much all
+    System.out.print("-----You have come far Guest and you will go farther with your pokemon  " + pokemon + " to raise, and your rival Christy to make you strive to do better.\n You will achieve greatness and defeat the odds... Guest you will be the world's best pokemon trainer.\n You will get " + pokemon + " to level " + level + ", because you are greatness ready to    rise.");
+    System.out.println(" ");
+    System.out.println("---------------Good luck & Welcome to the world of pokemon--------------");
+    
+
+    // Comments and final thoughts
+    // I really enjoyed doing this it was fun and I really enjoyed "scanner." I wish for the name of the player and rival, that I used a scanner, because it would allow more customization. I didn't use a scanner though, because I had little to know information about it. Thats one problem the other one is I wish the text would appear slower be
+
+
 
 
 
